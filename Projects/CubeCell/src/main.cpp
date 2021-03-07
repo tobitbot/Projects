5e38ddd3febcb7fa29a3363c2da24ccc9eef23c7
tobitbot <tobias@my-dogcity.de>
2021-03-07 15:07:31 +0100

    Added support for BH1750. Als support multiple sensors.

--- Projects/CubeCell/src/main.cpp
+++ Projects/CubeCell/src/main.cpp
@@ -5,6 +5,7 @@
 #include "Wire.h"
 
 #include <Adafruit_BME280.h>
+#include <BH1750.h>
 
 #define SEALEVELPRESSURE_HPA (1026.25) // this should be set according to the weather forecast
 
@@ -23,13 +24,9 @@ enum SensorType
     _NONE    = 0,
     _BME280  = 1,
     _BMP280  = 2,
-    _GY521   = 3
+    _GY521   = 3,
+    _BH1750  = 4
 };
-
-SensorType sensor = SensorType::_NONE;
-float temperature, humidity, pressure, altitude;
-
-Adafruit_BME280 bme;
 
 void OnSleep()
 {
@@ -53,41 +50,41 @@ void setDemoValues()
 {
     Serial.println("Set demo values");
     cayenne_lpp_add_digital_input(&lpp, appDataIndex++, 1);
-    cayenne_lpp_add_luminosity(&lpp, appDataIndex++, 250);
+    cayenne_lpp_add_digital_input(&lpp, appDataIndex++, 0);
+    cayenne_lpp_add_digital_input(&lpp, appDataIndex++, 1);
 }
 
 /**
- * Detect sensor type BME or BMP 280
+ * Detect sensor type and set the given place in the array of
+ * sensors.
  */
-SensorType getSensorType()
+int getSensorType(uint8_t address, SensorType* sensors, int index)
 {
-    bool isBME = bme.begin(0x76);
+    Adafruit_BME280 bme;
+    BH1750 lightMeter;
 
-    if (isBME)
+    Serial.println("");
+
+    if (bme.begin(address))
     {
-        if (isBME)
-        {
-            Serial.println("BME280 found!");
-            return SensorType::_BME280;
-        }
-        else
-        {
-            Serial.println("Error: 2, getSensorType()");
-            return SensorType::_NONE;
-        }
+        Serial.print(" BME280 found on address 0x");
+        Serial.println(address, HEX);
+        return sensors[index] = SensorType::_BME280;
     }
-    else if ((!isBME))
+    else if (lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE, address, &Wire))
     {
-        Serial.println("Could not find sensor");
-        return SensorType::_NONE;
+        Serial.print(" BH1750 found on address 0x");
+        Serial.println(address, HEX);
+        return sensors[index] = SensorType::_BH1750;
     }
     else
     {
-        Serial.println("Error: 2, getSensorType()");
-        return SensorType::_NONE;
+        Serial.print(" No valid sensor type found on address 0x ");
+        Serial.println(address, HEX);
+        return sensors[index] = SensorType::_NONE;
     }
 
-    return SensorType::_NONE;
+    return sensors[index] = SensorType::_NONE;
 }
 
 /**
@@ -95,36 +92,71 @@ SensorType getSensorType()
  */
 void readBME280()
 {
-    bme.readTemperature();
-    bme.readPressure();
-    bme.readHumidity();
+    Adafruit_BME280 bme;
+    Wire.begin();
 
-    delay(50);
+    if (bme.begin(0x76))
+    {
+        bme.readTemperature();
+        bme.readPressure();
+        bme.readHumidity();
 
-    temperature = bme.readTemperature();
-    pressure = bme.readPressure() / 100.0F;
-    altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-    humidity = bme.readHumidity();
+        delay(50);
 
-    cayenne_lpp_add_temperature(&lpp, appDataIndex++, temperature);
-    cayenne_lpp_add_barometric_pressure(&lpp, appDataIndex++, pressure);
-    cayenne_lpp_add_relative_humidity(&lpp, appDataIndex++, humidity);
+        float temperature = bme.readTemperature();
+        float pressure    = bme.readPressure() / 100.0F;
+        float altitude    = bme.readAltitude(SEALEVELPRESSURE_HPA);
+        float humidity    = bme.readHumidity();
+
+        cayenne_lpp_add_temperature(&lpp, appDataIndex++, temperature);
+        cayenne_lpp_add_barometric_pressure(&lpp, appDataIndex++, pressure);
+        cayenne_lpp_add_relative_humidity(&lpp, appDataIndex++, humidity);
+
+        char buf[256];
+        sprintf(buf, "BME 280 results:\n Temperature: %d °C Pressure: %d mBar Altitude: %d m Humdidity: %d \n",
+            static_cast<int>(temperature), static_cast<int>(pressure), static_cast<int>(altitude), static_cast<int>(humidity));
+        Serial.println(buf);
+    }
+    else
+    {
+        Serial.println("Error initialising BME280!");
+    }
+    Wire.end();
+}
+
+void readBH1750()
+{
+    BH1750 lightMeter(0x23);
+    Wire.begin();
+
+    if (lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE)) {
+        float luminosity = lightMeter.readLightLevel();
+
+        cayenne_lpp_add_luminosity(&lpp, appDataIndex++, luminosity);
+
+        char buf[100];
+        sprintf(buf, "BH1750 results:\n Luminosity: %d LUX \n", static_cast<int>(luminosity));
+        Serial.println(buf);
+    }
+    else {
+        Serial.println("Error initialising BH1750!");
+    }
     Wire.end();
 }
 
 /**
  * Scan I2C devices
+ *
+ * @return the number of sensors found
  */
-int scanI2C(bool &deviceFound)
+int scanI2C(SensorType* sensors)
 {
     Wire.begin(); // Wire communication begin
-    Serial.println("\nI2C Scanner");
 
     byte error, address; //variable for error and I2C address
     int nDevices;
 
-    Serial.println("Scanning...");
-    deviceFound = false;
+    Serial.println("Scanning I2C bus...");
 
     nDevices = 0;
     for (address = 1; address < 127; address++)
@@ -143,9 +175,8 @@ int scanI2C(bool &deviceFound)
                 Serial.print("0");
             }
             Serial.print(address, HEX);
-            Serial.println("  !");
+            getSensorType(address, sensors, nDevices);
             nDevices++;
-            deviceFound = true;
         }
         else if (error == 4)
         {
@@ -153,19 +184,20 @@ int scanI2C(bool &deviceFound)
             if (address < 16)
                 Serial.print("0");
             Serial.println(address, HEX);
-            deviceFound = false;
         }
     }
-    if (nDevices == 0)
+    if (nDevices <= 0)
     {
-        Serial.println("No I2C devices found\n");
+        Serial.println("No I2C devices found.");
     }
     else
     {
-        Serial.println("done\n");
+        Serial.print("Found ");
+        Serial.print(nDevices);
+        Serial.println(" devices on I2C bus.\n");
     }
     Wire.end();
-    return 0;
+    return nDevices;
 }
 
 /**
@@ -184,44 +216,38 @@ static void _print_buffer(cayenne_lpp_t *lpp)
 
     appDataSize = lpp->cursor;
     Serial.println(appDataSize);
-
-    Serial.print("Temperature = ");
-    Serial.print(temperature);
-    Serial.print("C, ");
-    Serial.print("Pressure = ");
-    Serial.print(pressure);
-    Serial.print("hPa, ");
-    Serial.print("Humidity = ");
-    Serial.print(humidity);
-    Serial.println("%");
 }
-
 
 void getSensorValues()
 {
-    // switch on external voltage
+    // switch on external voltage, inverse logic
     digitalWrite(Vext, LOW);
     delay(20);
-    bool devFound;
-    scanI2C(devFound);
-    Serial.println(devFound);
+
+    SensorType sensors[2] = {SensorType::_NONE, SensorType::_NONE};
+
+    int nDevices = scanI2C(sensors);
 
     cayenne_lpp_reset(&lpp);
 
-    sensor = getSensorType();
-    Serial.println(sensor);
-
-    switch (sensor)
+    for (int i = 0; i < sizeof(sensors); i++)
     {
-        case SensorType::_BME280 :
-            readBME280();
-            break;
-        case SensorType::_NONE:
-            setDemoValues();
-            break;
-        default:
-            setDemoValues();
-            break;
+        switch (sensors[i])
+        {
+            case SensorType::_NONE:
+                //Serial.print("Empty sensor slot: ");
+                //Serial.println(i);
+                break;
+            case SensorType::_BME280:
+                readBME280();
+                break;
+            case SensorType::_BH1750:
+                readBH1750();
+                break;
+            default:
+                setDemoValues();
+                break;
+        }
     }
     // switch off external voltage
     digitalWrite(Vext, HIGH);
